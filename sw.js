df6d5c2f82caf6c1240f3f42ebe0ca65a976f368
honoz <honoz@qq.com>
2020-03-17 22:16:15 +0800

    Site updated: 2020-03-17 22:16:08

--- sw.js
+++ sw.js
@@ -28,6 +28,8 @@ var precacheUrls = [];
 
   precacheUrls.push('/2018/11/19/nas服务器搭建/');
 
+  precacheUrls.push('/2020/03/17/gitbook/');
+
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
 
